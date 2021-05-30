a2d019bd6bd578fef1160244bb70de231381e5ed
rishcyberspace <rishabh.r.gupta@accenture.com>
2021-05-30 21:34:43 +0530

    Adding a dummy project

---
+++ src/main/java/testing/hello/App.java
@@ -0,0 +1,13 @@
+package testing.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from Eclipse IDE" );
+    }
+}
